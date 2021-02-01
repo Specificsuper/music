e39e4fa8d4bb0f5b2f5f10f1719c06cf647a619c
daniel <mydanielz@163.com>
2021-02-01 23:27:55 +0800

    init

---
+++ precache-manifest.8486877273cbe302c847825a74ce5554.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2609b1700bbc7cb05f",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-06bd35c2.3c3894ff.css"
+  },
+  {
+    "revision": "0ec8964de908b2d0cbd1",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-15ed0b58.f10a7514.css"
+  },
+  {
+    "revision": "d7ab74ebdcedc5686309",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-1cc1ed98.ed374ef2.css"
+  },
+  {
+    "revision": "059a0a664f68f4865535",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-263590a8.bb0ce2f9.css"
+  },
+  {
+    "revision": "6ba7ceb896a51617e370",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-3f6f9db1.d0abc55d.css"
+  },
+  {
+    "revision": "a80d9f60935c280ba12a",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-43a738f4.ff31e5af.css"
+  },
+  {
+    "revision": "6aab38e88e187e2e71c9",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-60636be5.51fc0119.css"
+  },
+  {
+    "revision": "1826704f04c605eeb39d",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-675deb54.7e761518.css"
+  },
+  {
+    "revision": "948934bbdd7227880971",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-69ff310d.e0326e71.css"
+  },
+  {
+    "revision": "70d18219d4b748998bc6",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-6be815c2.babd9435.css"
+  },
+  {
+    "revision": "bdd8283f2b976ebd83f4",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-76f4fb7a.5fb4548a.css"
+  },
+  {
+    "revision": "f84592532c8d10a50278",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-9077684c.ace07773.css"
+  },
+  {
+    "revision": "8d6e7c7c3c84ae8e874e",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-929e7ba4.8b69a84d.css"
+  },
+  {
+    "revision": "e0080393cf387a94a397",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-af381e96.9f84654d.css"
+  },
+  {
+    "revision": "b91d5389f6662da3a52a",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-b1cac712.42e9e76d.css"
+  },
+  {
+    "revision": "1476a016a729744b58b9",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-c1361de0.cf737bd1.css"
+  },
+  {
+    "revision": "94c1fd32f8aaa9b8fe59",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/chunk-da59ceb8.351a82ce.css"
+  },
+  {
+    "revision": "62bb1beaa69f82bbc13f",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/css/index.0c98c6df.css"
+  },
+  {
+    "revision": "e1e193ddd5b29a6605178bdd8d134246",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/img/logos/netease-music.png"
+  },
+  {
+    "revision": "2822cbcf68de083b96ac3921d0e308a2",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/img/logos/nyancat.gif"
+  },
+  {
+    "revision": "e099cb8ea828a912212f9022b96d248f",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/img/logos/yesplaymusic.png"
+  },
+  {
+    "revision": "96f6b0fe04aac7d3f5829210ff667221",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/index.html"
+  },
+  {
+    "revision": "cc2609b1700bbc7cb05f",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-06bd35c2.dd965a75.js"
+  },
+  {
+    "revision": "0ec8964de908b2d0cbd1",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-15ed0b58.57758576.js"
+  },
+  {
+    "revision": "d7ab74ebdcedc5686309",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-1cc1ed98.819cc4cd.js"
+  },
+  {
+    "revision": "059a0a664f68f4865535",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-263590a8.0d4b3ebc.js"
+  },
+  {
+    "revision": "6ba7ceb896a51617e370",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-3f6f9db1.050faf0d.js"
+  },
+  {
+    "revision": "a80d9f60935c280ba12a",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-43a738f4.f132cc80.js"
+  },
+  {
+    "revision": "6aab38e88e187e2e71c9",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-60636be5.aedb893b.js"
+  },
+  {
+    "revision": "1826704f04c605eeb39d",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-675deb54.448ee213.js"
+  },
+  {
+    "revision": "948934bbdd7227880971",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-69ff310d.e00ec582.js"
+  },
+  {
+    "revision": "70d18219d4b748998bc6",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-6be815c2.3e537e3e.js"
+  },
+  {
+    "revision": "bdd8283f2b976ebd83f4",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-76f4fb7a.e5deca0d.js"
+  },
+  {
+    "revision": "f84592532c8d10a50278",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-9077684c.e4eef411.js"
+  },
+  {
+    "revision": "8d6e7c7c3c84ae8e874e",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-929e7ba4.2f36ddbc.js"
+  },
+  {
+    "revision": "e0080393cf387a94a397",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-af381e96.5e5a043a.js"
+  },
+  {
+    "revision": "b91d5389f6662da3a52a",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-b1cac712.504fb248.js"
+  },
+  {
+    "revision": "1476a016a729744b58b9",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-c1361de0.e13e1a68.js"
+  },
+  {
+    "revision": "94c1fd32f8aaa9b8fe59",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-da59ceb8.bb8a31f7.js"
+  },
+  {
+    "revision": "ae79ab319c8d7790466f",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/chunk-vendors.1e6384d0.js"
+  },
+  {
+    "revision": "62bb1beaa69f82bbc13f",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/js/index.25948d09.js"
+  },
+  {
+    "revision": "b9d79419c8051857dbc1dedbc00e2ab2",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://cdn.jsdelivr.net/gh/doourdo27/music@1.2/robots.txt"
+  }
+]);
